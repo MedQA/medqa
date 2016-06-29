aca9ff45c17565ec92b6f688e135624ead73cc47
Himanshi-Khandelwal <himanshikhandelwal8@gmail.com>
2016-06-29 13:03:19 +0530

    frontend

--- app/api/helloworld.py
+++ app/api/helloworld.py
@@ -1,15 +1,16 @@
 """
  Simple API endpoint for returning helloworld
 """
-from flask import (Blueprint, render_template, current_app, request,  
+from flask import (Blueprint, render_template, current_app, request,Flask,
                    flash, url_for, redirect, session, abort, jsonify, make_response)
 
 helloworld = Blueprint('helloworld', __name__, url_prefix='/api/helloworld')
 
 @helloworld.route('/helloworld', methods=['GET'])
-def index():
 
-   data = {
-      'message' : "helloworld"
-   }      
+data = {
+     'message' : "helloworld"
+   }
    return make_response(jsonify(data))
+def index():
+   return render_template('index.html')
